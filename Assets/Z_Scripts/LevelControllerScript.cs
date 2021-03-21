19287620144893b3a7a926cafd8d5951ee7daefe
Zack <1015688@student.sae.edu>
2021-03-21 23:36:01 +1100

    Particles
    Folder:
    - Scripts
    -Materials
    
    Particles:
    - Walking Bot Explosion
    - Trail Particle; Enemy projectiles (Player projectiles?)
    - Enemy dmg effect
    
    Scripts:
    - ProjectileScript
    - ProjectileShotScript
    - FlashImageScript
    - Level controller
    
    UI:
    - Flash Image [Player dmg]

---
+++ Assets/Z_Scripts/LevelControllerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelControllerScript : MonoBehaviour
+{
+
+	[SerializeField] FlashImageScript _flashImage = null;
+	[SerializeField] Color _newColor = Color.red;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+	{
+		_flashImage.StartFlash(.25f, .5f, _newColor);
+	}
+    }
+}
