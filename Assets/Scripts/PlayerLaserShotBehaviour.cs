f027f01fb6122a9ecb9765e6a79ce99eb2147af7
Seth Reay <1017464@student.sae.edu.au>
2021-03-24 17:13:15 +1100

    APK Prototype Ready

---
+++ Assets/Scripts/PlayerLaserShotBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLaserShotBehaviour : MonoBehaviour
+{
+    public float velocity;
+
+    private void Update()
+    {
+        transform.position += transform.forward * velocity * Time.deltaTime;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.transform.tag == "FlyingBot")
+        {
+            Destroy(other.gameObject);
+        }
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/ProjectileScript.cs
+++ Assets/Scripts/ProjectileScript.cs
@@ -8,20 +8,13 @@ public class ProjectileScript : MonoBehaviour
 	public GameObject projectileShot;
 	public float shootForce = 0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
-	{
-		GameObject projectile = (GameObject)Instantiate(
-			projectileShot, transform.position, transform.rotation);
-		projectile.GetComponent<Rigidbody>().AddForce(projectile.transform.forward * shootForce);
-	}
+		{
+			GameObject projectile = (GameObject)Instantiate(projectileShot, transform.position, transform.rotation);
+			projectile.GetComponent<Rigidbody>().AddForce(projectile.transform.forward * shootForce);
+		}
     }
 }
