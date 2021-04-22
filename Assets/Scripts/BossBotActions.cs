6f0da846ee9ecaca4c8bf0c2636592f970b87628
Seth Reay <1017464@student.sae.edu.au>
2021-04-22 11:36:41 +1000

    Added BossBot, Added smog, Pushed enemy spawns back

---
+++ Assets/Scripts/BossBotActions.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBotActions : MonoBehaviour
+{
+    public int hp;
+    public float speed;
+    public GameObject destination;
+
+    void Update()
+    {
+        if (hp > 0){Move();}
+        if (Vector3.Distance(transform.position, destination.transform.position) > 0.1f)
+        {
+            Move();
+        }
+    }
+    void Move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, destination.transform.position, speed * Time.deltaTime);
+    }
+    public void Damage()
+    {
+        //Flash red
+        hp--;
+        if (hp <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+        //Expolode
+        //Large smoke effect
+        //Call game ending
+    }
+}
